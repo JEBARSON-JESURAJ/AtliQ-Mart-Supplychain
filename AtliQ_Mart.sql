4cab7015624dfbbb88d3f6d76b1f0bc4339b691b
Jebarson Jesuraj <74528651+JEBARSON-JESURAJ@users.noreply.github.com>
2023-07-23 17:51:08 +0400

    Update AtliQ_Mart.sql

--- AtliQ_Mart.sql
+++ AtliQ_Mart.sql
@@ -29,3 +29,17 @@ FROM fact_order_lines AS ol
 INNER JOIN dim_customers AS c
 USING(customer_id)
 GROUP BY c.city;
+
+-- Getting Target and Actual together
+SELECT tar.customer_id, tar."infull_target%", sq."Actual In_Full", tar."ontime_target%", sq."Actual On_Time", tar."ontime_target%",sq."Actual On Time In_Full"
+FROM dim_targets_orders AS tar
+INNER JOIN (
+    SELECT ol.customer_id, c.customer_name,
+        (ROUND(AVG(ol."In Full"),2))*100 AS "Actual In_Full",
+        (ROUND(AVG(ol."On Time"),2))*100 AS "Actual On_Time",
+        (ROUND(AVG(ol."On Time In Full"),2))*100 AS "Actual On Time In_Full"
+    FROM fact_order_lines AS ol
+    INNER JOIN dim_customers AS c
+    USING(customer_id) 
+    GROUP BY ol.customer_id, c.customer_name) AS sq
+USING (customer_id);
