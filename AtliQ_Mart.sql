271c94ba0b6689edf87d0ed676ef276033d91bc8
Jebarson Jesuraj <74528651+JEBARSON-JESURAJ@users.noreply.github.com>
2023-07-23 20:16:19 +0400

    metrics customer & city wise (Target Vs Actual)

--- AtliQ_Mart.sql
+++ AtliQ_Mart.sql
@@ -30,16 +30,40 @@ INNER JOIN dim_customers AS c
 USING(customer_id)
 GROUP BY c.city;
 
--- Getting Target and Actual together
-SELECT tar.customer_id, tar."infull_target%", sq."Actual In_Full", tar."ontime_target%", sq."Actual On_Time", tar."ontime_target%",sq."Actual On Time In_Full"
+-- Getting metrics customerwise (Target Vs Actual)
+SELECT tar.customer_id, sq.customer_name,
+    tar."infull_target%" AS "Target_Infull", sq."Actual_InFull",
+    tar."ontime_target%" AS "Target_OnTime", sq."Actual_OnTime",
+    tar."ontime_target%" AS "Target_OnTime_InFull",sq."Actual_OnTime_InFull"
 FROM dim_targets_orders AS tar
 INNER JOIN (
     SELECT ol.customer_id, c.customer_name,
-        (ROUND(AVG(ol."In Full"),2))*100 AS "Actual In_Full",
-        (ROUND(AVG(ol."On Time"),2))*100 AS "Actual On_Time",
-        (ROUND(AVG(ol."On Time In Full"),2))*100 AS "Actual On Time In_Full"
+        (ROUND(AVG(ol."In Full"),2))*100 AS "Actual_InFull",
+        (ROUND(AVG(ol."On Time"),2))*100 AS "Actual_OnTime",
+        (ROUND(AVG(ol."On Time In Full"),2))*100 AS "Actual_OnTime_InFull"
     FROM fact_order_lines AS ol
     INNER JOIN dim_customers AS c
     USING(customer_id) 
     GROUP BY ol.customer_id, c.customer_name) AS sq
 USING (customer_id);
+
+-- Getting metrics city wise (Target Vs Actual)
+SELECT sq.city,
+    ROUND(AVG(tar."infull_target%"),2) AS "Target_Infull",
+    ROUND(AVG(sq."Actual_InFull"),2) AS "Actual_InFull",
+    ROUND(AVG(tar."ontime_target%"),2)AS "Target_OnTime",
+    ROUND(AVG(sq."Actual_OnTime"),2) AS "Actual_OnTime",
+    ROUND(AVG(tar."ontime_target%"),2) AS "Target_OnTime_InFull",
+    ROUND(AVG(sq."Actual_OnTime_InFull"),2) AS "Actual_OnTime_InFull"
+FROM dim_targets_orders AS tar
+INNER JOIN (
+    SELECT ol.customer_id, c.city,
+        (ROUND(AVG(ol."In Full"),2))*100 AS "Actual_InFull",
+        (ROUND(AVG(ol."On Time"),2))*100 AS "Actual_OnTime",
+        (ROUND(AVG(ol."On Time In Full"),2))*100 AS "Actual_OnTime_InFull"
+    FROM fact_order_lines AS ol
+    INNER JOIN dim_customers AS c
+    USING(customer_id) 
+    GROUP BY ol.customer_id,c.city) AS sq
+USING (customer_id)
+GROUP BY sq.city;
