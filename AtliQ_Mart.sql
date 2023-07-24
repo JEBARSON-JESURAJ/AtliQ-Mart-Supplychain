ee9cd9058c96cf53af3106677a70bb62fd824d4c
Jebarson Jesuraj <74528651+JEBARSON-JESURAJ@users.noreply.github.com>
2023-07-24 20:21:42 +0400

    deleted queries that no longer need

--- AtliQ_Mart.sql
+++ AtliQ_Mart.sql
@@ -1,7 +1,3 @@
-SELECT * FROM dim_customers
---Group by actual_delivery_date
-LIMIT 5;
-
 --OT, IF, OTIF Metrics on daily basis
  
 SELECT actual_delivery_date, (ROUND(AVG("In Full"),2))*100 AS "In_Full",
@@ -9,26 +5,6 @@ SELECT actual_delivery_date, (ROUND(AVG("In Full"),2))*100 AS "In_Full",
     (ROUND(AVG("On Time In Full"),2))*100 AS "On Time In_Full"
 FROM fact_order_lines
 GROUP BY actual_delivery_date;
-
---Customer wise metrics
-SELECT ol.customer_id, c.customer_name,
-    (ROUND(AVG(ol."In Full"),2))*100 AS "In_Full",
-    (ROUND(AVG(ol."On Time"),2))*100 AS "On_Time",
-    (ROUND(AVG(ol."On Time In Full"),2))*100 AS "On Time In_Full"
-FROM fact_order_lines AS ol
-INNER JOIN dim_customers AS c
-USING(customer_id)
-GROUP BY ol.customer_id, c.customer_name;
-
---City wise metrics
-SELECT c.city,
-    (ROUND(AVG(ol."In Full"),2))*100 AS "In_Full",
-    (ROUND(AVG(ol."On Time"),2))*100 AS "On_Time",
-    (ROUND(AVG(ol."On Time In Full"),2))*100 AS "On Time In_Full"
-FROM fact_order_lines AS ol
-INNER JOIN dim_customers AS c
-USING(customer_id)
-GROUP BY c.city;
 
 -- Getting metrics customerwise (Target Vs Actual)
 SELECT tar.customer_id, sq.customer_name,
